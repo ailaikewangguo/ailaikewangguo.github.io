7dd96ad795ee30801912e9ea23e325b9959296ac
e <ailaikewangguo@tutanota.com>
2018-10-27 15:00:09 +0800

    Site updated: 2018-10-27 15:00:08

--- js/src/1.js
+++ js/src/1.js
@@ -1,5 +1,4 @@
 $(function() {
-console.warn("%c 想看本站代码的话 %c","background:#24272A; color:#ffffff","","https://xn--74qs2lt61alvn4mb.website/");
-console.error("%c 不如来本公会来看看吧~ %c","background:#24272A; color:#ffffff","","https://xn--74qs2lt61alvn4mb.website/");
-console.info("%c 还请希望扒站的大佬手下留情 %c","background:#24272A; color:#ffffff","","https://xn--74qs2lt61alvn4mb.website/");
+console.warn("%c 想看本站代码的话，不如来本公会来看看吧~ %c","background:#24272A; color:#ffffff","","https://xn--74qs2lt61alvn4mb.website/");
+console.error("%c 还请希望扒站的大佬手下留情 %c","background:#24272A; color:#ffffff","","https://xn--74qs2lt61alvn4mb.website/");
 });
