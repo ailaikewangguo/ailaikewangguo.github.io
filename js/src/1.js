81fc6f85462b4b6c8a0c2de3a34adf266c7b95fc
e <ailaikewangguo@tutanota.com>
2018-10-27 14:32:57 +0800

    Site updated: 2018-10-27 14:32:56

---
+++ js/src/1.js
@@ -0,0 +1,5 @@
+$(function() {
+console.warn("%c 想看本站代码的话 %c","background:#24272A; color:#ffffff","","https://cangshui.net/");
+console.error("%c 不如来本公会来看看吧~ %c","background:#24272A; color:#ffffff","","https://cangshui.net/");
+console.info("%c 还请希望扒站的大佬手下留情 %c","background:#24272A; color:#ffffff","","https://cangshui.net/");
+});
